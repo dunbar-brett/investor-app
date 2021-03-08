9f0193e31ea1b7d0c8220f2f6514d29e2894ce6f
dunbar-brett <dunbar.brett@gmail.com>
2021-03-07 16:47:23 -0800

    more form stuff

--- src/components/InvestorForm.js
+++ src/components/InvestorForm.js
@@ -5,8 +5,18 @@ import { useForm } from 'react-hook-form'
 
 
 export default function InvestorForm (props) {
+    const {register, handleSubmit, errors } = useForm();
+
+    const onSubmit = data => alert(JSON.stringify(data));
+
 
     return (
-        <h1>Hello!</h1>
+        <div>
+            <h1>Please Enter Info Below</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="text"/>
+            </form>
+
+        </div>
     )
 }
