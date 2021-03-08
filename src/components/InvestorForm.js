63dee1f55549c8fb1df2b66ce0e5fd700062daaa
dunbar-brett <dunbar.brett@gmail.com>
2021-03-07 17:18:31 -0800

    more forms with validation and error messages

--- src/components/InvestorForm.js
+++ src/components/InvestorForm.js
@@ -7,14 +7,56 @@ import { useForm } from 'react-hook-form'
 export default function InvestorForm (props) {
     const {register, handleSubmit, errors } = useForm();
 
-    const onSubmit = data => alert(JSON.stringify(data));
+    const onSubmit = (data) => alert(JSON.stringify(data));
 
 
     return (
         <div>
             <h1>Please Enter Info Below</h1>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <input type="text"/>
+                <label>
+                    Investment Amount
+                    <input name="investmentAmount" type="number" ref={register({required: true})}/>
+                    {errors.investmentAmount && <span>Investment Amount is Required</span>}
+                </label>
+                <br/>
+                <label>
+                    Investment Type
+                    <input name="investmentType" type="text" ref={register({required: true})}/>
+                    <span>{errors.investmentType && "Investment Type is Required"}</span>
+                </label>
+                <br/>
+                <label>
+                    Total Net Worth
+                    <input name="totalNetWorth" type="number" ref={register({required: true})}/>
+                    <span>{errors.investmentType && "Investment Type is Required"}</span>
+                </label>
+                <br/>
+                <label>
+                    Estimated Yearly Income
+                    <input name="investmentType" type="text" ref={register({required: true})}/>
+                    <span>{errors.investmentType && "Investment Type is Required"}</span>
+                </label>
+                <br/>
+                <label>
+                    Estimated Credit Score
+                    <input name="estimatedCreditScore" type="number"
+                        ref={register({
+                            required: "Estimated Credit Score is required", 
+                            min: {
+                                value: 300,
+                                message: "Minimum credit score is 300."
+                            }, 
+                            max: {
+                                value: 850,
+                                message: "Maximum credit score is 850."
+                            }
+                        })
+                    }/>
+                    {errors.estimatedCreditScore?.message}
+                </label>
+                <br/>
+                <input type="submit" />
             </form>
 
         </div>
